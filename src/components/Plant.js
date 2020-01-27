575da76c3d70a3ba9575b3a8fa66b2fde191336c
song <songeun.you7@gmail.com>
2020-01-26 21:31:18 -0500

    Added fifth and sixth plant

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -74,6 +74,49 @@ class Plant4 extends Component {
     }
 }
 
+class Plant5 extends Component {
+    render() {
+        return(
+            <div className="plant plant5">
+                <div className="stem"/>
+                <div className="leaf left-leaf">
+                    <div className="stripe"/>
+                    <div className="stripe"/>
+                    <div className="stripe"/>
+                </div>
+                <div className="leaf right-leaf">
+                    <div className="stripe"/>
+                    <div className="stripe"/>
+                    <div className="stripe"/>
+                </div>
+                <div className="flower">
+                    <div className="petal5"/>
+                    <div className="inner-petal petal3"/>
+                    <div className="inner-petal petal4"/>
+                    <div className="petal petal1"/>
+                    <div className="petal petal2"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Plant6 extends Component {
+    render() {
+        return(
+            <div className="plant plant6">
+                <div className="flower"/>
+                <div className="leaf leaf1"/>
+                <div className="leaf leaf2"/>
+                <div className="leaf leaf3">
+                    <div className="inner-leaf leaf1"/>
+                    <div className="inner-leaf leaf2"/>
+                </div>
+            </div>
+        );
+    }
+}
+
 class Plant extends Component {
     constructor(props) {
         super(props);
@@ -92,7 +135,7 @@ class Plant extends Component {
 
     randomPlant() {
         const min=0;
-        const max=3;
+        const max=5;
         const random = Math.round(Math.random() * (+max - +min) + +min);
 
         if (random === 0) {
@@ -103,6 +146,10 @@ class Plant extends Component {
             return <Plant3/>
         } else if (random === 3) {
             return <Plant4/>
+        } else if (random === 4) {
+            return <Plant5/>
+        } else if (random === 5) {
+            return <Plant6/>
         }
     }
 
