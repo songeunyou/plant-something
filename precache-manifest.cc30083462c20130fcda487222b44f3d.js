9e834f65dc366224da95b2dcb5e31d60f79926d7
song <songeun.you7@gmail.com>
2020-04-12 23:00:40 -0400

    Updates

---
+++ precache-manifest.cc30083462c20130fcda487222b44f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fa676cd11ffd884e2e0",
+    "url": "/plant-something/static/css/main.14ac0987.chunk.css"
+  },
+  {
+    "revision": "9fa676cd11ffd884e2e0",
+    "url": "/plant-something/static/js/main.24383bf1.chunk.js"
+  },
+  {
+    "revision": "fa4df43abfdbc4796574",
+    "url": "/plant-something/static/js/runtime~main.533a291d.js"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/plant-something/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "38753e6c7e7472005bdd608182480d39",
+    "url": "/plant-something/static/media/heart.38753e6c.png"
+  },
+  {
+    "revision": "c5f65dde7b461dec1d437cbfa678492c",
+    "url": "/plant-something/index.html"
+  }
+];
